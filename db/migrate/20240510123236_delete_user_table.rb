7173ae2f7517a7cd17f7fb0bd077bf9c4c53a13f
Subash R <waytosubash@gmail.com>
2024-05-10 18:03:45 +0530

    Drop Table for devise gem issue

---
+++ db/migrate/20240510123236_delete_user_table.rb
@@ -0,0 +1,5 @@
+class DeleteUserTable < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_144504) do
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2024_05_10_123236) do
 end
