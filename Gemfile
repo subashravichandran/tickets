b603187f450aedbc89c60c88c901fd74b91cac32
Subash R <waytosubash@gmail.com>
2024-05-10 18:16:10 +0530

    Devise login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.3.0"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
+gem 'devise'
 gem 'rack-cors'
 
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  namespace :api do
-    namespace :v1 do
-      resources :users
-    end
-  end
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
