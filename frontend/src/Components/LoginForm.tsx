491168358dcc6e48db8cbbb6d018481308fc70af
Subash R <waytosubash@gmail.com>
2024-05-12 13:41:24 +0530

    localstorage

--- frontend/src/Components/LoginForm.tsx
+++ frontend/src/Components/LoginForm.tsx
@@ -1,6 +1,6 @@
 // import React from React
 
-import { useDebugValue, useState } from "react"
+import { useDebugValue, useEffect, useState } from "react"
 import axios from "axios";
 
 interface FromProps {
@@ -11,10 +11,21 @@ interface FromProps {
   setPassword: React.Dispatch<React.SetStateAction<string>>;
 }
 
+const setLocalStorage = (data: any) => {
+  localStorage.setItem('login-id', data.id)
+  localStorage.setItem('login-hash', data.jti)
+}
+
+const clearLocalStorage = () => {
+  localStorage.removeItem('login-id')
+  localStorage.removeItem('login-hash')
+}
+
 const loginUser = async (email: string, password: string) => {
   try {
     const response = await axios.post('http://127.0.0.1:3000/login', { user: { email, password }});
     console.log(response.data); // Log successful response data
+    setLocalStorage(response.data.data)
     return response.data; // Return the response data
   } catch (error: any) {
     console.error("Error occurred:", error.response?.data); // Log detailed error information
@@ -27,10 +38,21 @@ function LoginForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  useEffect(() => {
+    if (localStorage.getItem('login-hash')) {
+      setIsLoggedIn(true)
+    }
+  })
+
   const handleLoginClick = () => {
     const userData = loginUser(email, password);
     console.log(userData)
     setIsLoggedIn(true)
+  }
+
+  const handleLogoutClick = () => {
+    clearLocalStorage()
+    setIsLoggedIn(false)
   }
 
   return (
@@ -41,7 +63,7 @@ function LoginForm() {
         password = {password}
         setEmail = {setEmail}
         setPassword = {setPassword} />
-      { isLoggedIn ? null : <button onClick={handleLoginClick}>LOGIN</button> }
+      { isLoggedIn ? <button onClick={handleLogoutClick}>LOGOUT</button> : <button onClick={handleLoginClick}>LOGIN</button> }
     </>
   )
 }
