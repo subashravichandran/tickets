90a3eb88237eaee14b108b86056d95248f62a199
Subash R <waytosubash@gmail.com>
2024-06-15 15:12:13 +0530

    Toggle icon to text

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,16 +1,27 @@
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { faGear, faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Container, Nav, NavDropdown, Navbar, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 function NavBar() {
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+  const handleMouseEnter = (item :string) => { setHoveredItem(item) }
+  const handleMouseLeave = () => { setHoveredItem(null) }
+
   return (
     <Navbar bg='primary' data-bs-theme='light'>
       <Container fluid>
         <Navbar.Brand href="/">Dashboard</Navbar.Brand>
           <Nav>
             <LinkContainer to="/todo_lists">
-              <Nav.Link>Todo's</Nav.Link>
+              <Nav.Link onMouseEnter={ () => handleMouseEnter('todo') } onMouseLeave={ handleMouseLeave }>
+                { hoveredItem === 'todo' ? 'Todo\'s' : <FontAwesomeIcon icon={ faListCheck } /> }
+              </Nav.Link>
             </LinkContainer>
-            <NavDropdown title='Settings' id='app-settings'>
+            <NavDropdown onMouseEnter={ () => handleMouseEnter('settings') }
+                         onMouseLeave={ handleMouseLeave }
+                         title={ hoveredItem === 'settings' ? 'Settings' : <FontAwesomeIcon icon={faGear} />} id='app-settings'>
               <LinkContainer to='/uoms'>
                 <NavDropdown.Item as='span'>UOM</NavDropdown.Item>
               </LinkContainer>
