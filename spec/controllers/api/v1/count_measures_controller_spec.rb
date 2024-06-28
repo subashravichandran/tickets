6ba1452f330750bbc97af2941dfe23094b9d35c8
Subash R <waytosubash@gmail.com>
2024-06-28 09:10:58 +0530

    habits testcases

--- spec/controllers/api/v1/count_measures_controller_spec.rb
+++ spec/controllers/api/v1/count_measures_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Api::V1::CountMeasuresController, type: :controller do
   describe 'POST #create', :skip_before_each do
     it 'creates a new CountMeasure' do
       expect{
-        post :create, params: { count_measure: valid_uom}
+        post :create, params: { count_measure: valid_uom }
       }.to change(CountMeasure, :count).by(1)
       expect(response).to have_http_status(:created)
     end
